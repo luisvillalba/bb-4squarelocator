af915c64132c79c21ddd472ab906e089a4025ced
Luis Villalba <luismvillalba@gmail.com>
2016-08-02 19:37:03 -0500

    Fixing code format

--- install/Gruntfile.js
+++ install/Gruntfile.js
@@ -1,16 +1,13 @@
 module.exports = function (grunt) {
+  grunt.initConfig({
+    auto_install: {
+      local: {
+        npm: true
+      }
+    }
+  });
 
-    grunt.initConfig({
-        auto_install: {
-            local: {
-                npm: true
-            }
-        }
-    });
-
-    grunt.loadNpmTasks('grunt-auto-install');
-    
-    grunt.registerTask('default', []);
-    grunt.registerTask('install', ['auto_install']);
-
+  grunt.loadNpmTasks('grunt-auto-install');
+  grunt.registerTask('default', []);
+  grunt.registerTask('install', ['auto_install']);
 };
--- src/js/views/popularView.js
+++ src/js/views/popularView.js
@@ -1,32 +1,29 @@
 define([
-        'backbone',
-        'handlebars',
-        'text!views/templates/popularView.hbs',
-    ], 
-    function (Backbone, Handlebars, template) {
-        'use strict';
-    
-        var PopularView = Backbone.View.extend({
-            "tagName": "div",
-            
-            "template": Handlebars.compile(template),
-            
-            "className": "popularContainer",
-            
-            "initialize": function() {
-                if (this.model.get('size') == 'small') {
-                    this.$el.addClass('col-xs-6 col-sm-3 col-lg-3');
-                } else {
-                    this.$el.addClass('col-xs-12 col-sm-6 col-lg-6');
-                }
-            },
-            
-            "render": function() {
-                this.$el.html(this.template(this.model.attributes));
-                return this;
-            }
-        });
-    
-        return PopularView;
-    }
+    'backbone',
+    'handlebars',
+    'text!views/templates/popularView.hbs',
+  ], 
+  function (Backbone, Handlebars, template) {
+    'use strict';
+    var PopularView = Backbone.View.extend({
+      "tagName": "div",
+      "template": Handlebars.compile(template),
+      "className": "popularContainer",
+      "initialize": function() {
+        if (this.model.get('size') == 'small') {
+          this.$el.addClass('col-xs-6 col-sm-3 col-lg-3');
+        } else {
+          this.$el.addClass('col-xs-12 col-sm-6 col-lg-6');
+        }
+      },
+
+      "render": function() {
+        this.$el.html(this.template(this.model.attributes));
+
+        return this;
+      }
+    });
+
+    return PopularView;
+  }
 );
